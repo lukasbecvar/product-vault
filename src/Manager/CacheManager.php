933dc80805a53468147606801035df755929c9ef
Lukáš Bečvář <lordbecvold@gmail.com>
2024-12-12 16:23:45 +0100

    added cache manager (redis cache)

---
+++ src/Manager/CacheManager.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Manager;
+
+use Exception;
+use Predis\Client;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Class CacheManager
+ *
+ * The manager for get and manage cache storage
+ *
+ * @package App\Manager
+ */
+class CacheManager
+{
+    private Client $redis;
+    private ErrorManager $errorManager;
+
+    public function __construct(Client $redis, ErrorManager $errorManager)
+    {
+        $this->redis = $redis;
+        $this->errorManager = $errorManager;
+    }
+
+    /**
+     * Save value to cache storage
+     *
+     * @param string $key The cache key
+     * @param string $value The cache value
+     * @param int $expirationTTL The cache expiration time in seconds
+     *
+     * @return void
+     */
+    public function saveCacheValue(string $key, string $value, int $expirationTTL = 60): void
+    {
+        try {
+            $this->redis->set($key, $value, 'EX', $expirationTTL);
+        } catch (Exception $e) {
+            $this->errorManager->handleError(
+                message: 'error to save cache value',
+                code: JsonResponse::HTTP_INTERNAL_SERVER_ERROR,
+                exceptionMessage: $e->getMessage()
+            );
+        }
+    }
+
+    /**
+     * Check if cache value exists in storage
+     *
+     * @param string $key The cache key
+     *
+     * @return bool True if cache value exists, false otherwise
+     */
+    public function checkIsCacheValueExists(string $key): bool
+    {
+        try {
+            $status = $this->redis->exists($key);
+        } catch (Exception $e) {
+            $this->errorManager->handleError(
+                message: 'error to check cache value exists',
+                code: JsonResponse::HTTP_INTERNAL_SERVER_ERROR,
+                exceptionMessage: $e->getMessage()
+            );
+        }
+
+        // check if cache value exists
+        if ($status == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get cache value from storage
+     *
+     * @param string $key The cache key
+     *
+     * @return string|null The cache value if exists, null otherwise
+     */
+    public function getCacheValue(string $key): ?string
+    {
+        try {
+            return $this->redis->get($key);
+        } catch (Exception $e) {
+            $this->errorManager->handleError(
+                message: 'error to get cache value',
+                code: JsonResponse::HTTP_INTERNAL_SERVER_ERROR,
+                exceptionMessage: $e->getMessage()
+            );
+        }
+    }
+
+    /**
+     * Delete cache value from storage
+     *
+     * @param string $key The cache key
+     *
+     * @return void
+     */
+    public function deleteCacheValue(string $key): void
+    {
+        try {
+            $this->redis->del($key);
+        } catch (Exception $e) {
+            $this->errorManager->handleError(
+                message: 'error to delete cache value',
+                code: JsonResponse::HTTP_INTERNAL_SERVER_ERROR,
+                exceptionMessage: $e->getMessage()
+            );
+        }
+    }
+}
---
+++ tests/Manager/CacheManagerTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Tests\Manager;
+
+use Predis\Client;
+use App\Manager\CacheManager;
+use App\Manager\ErrorManager;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+
+/**
+ * Class CacheManagerTest
+ *
+ * Test cases for cache manager
+ *
+ * @package App\Tests\Manager
+ */
+class CacheManagerTest extends TestCase
+{
+    private CacheManager $cacheManager;
+    private Client & MockObject $redis;
+    private ErrorManager & MockObject $errorManager;
+
+    public function setUp(): void
+    {
+        // mock dependencies
+        $this->redis = $this->createMock(Client::class);
+        $this->errorManager = $this->createMock(ErrorManager::class);
+
+        // init cache manager instance
+        $this->cacheManager = new CacheManager($this->redis, $this->errorManager);
+    }
+
+    /**
+     * Test save cache value
+     *
+     * @return void
+     */
+    public function testSaveCacheValue(): void
+    {
+        // expect call set cache value
+        $this->redis->expects($this->once())->method('__call')
+            ->with('set', ['test_key', 'test_value', 'EX', 60]);
+
+        // expect no error handling
+        $this->errorManager->expects($this->never())->method('handleError');
+
+        // call tested method
+        $this->cacheManager->saveCacheValue('test_key', 'test_value');
+    }
+
+    /**
+     * Test save cache value with error handling
+     *
+     * @return void
+     */
+    public function testCheckIsCacheValueExists(): void
+    {
+        // expect call check cache value exists
+        $this->redis->expects($this->once())->method('__call')
+            ->with('exists', ['test_key']);
+
+        // expect no error handling
+        $this->errorManager->expects($this->never())->method('handleError');
+
+        // call tested method
+        $this->cacheManager->checkIsCacheValueExists('test_key');
+    }
+
+    /**
+     * Test check is cache value exists
+     *
+     * @return void
+     */
+    public function testGetCacheValue(): void
+    {
+        // expect call get cache value
+        $this->redis->expects($this->once())->method('__call')
+            ->with('get', ['test_key']);
+
+        // expect no error handling
+        $this->errorManager->expects($this->never())->method('handleError');
+
+        // call tested method
+        $this->cacheManager->getCacheValue('test_key');
+    }
+
+    /**
+     * Test delete cache value
+     *
+     * @return void
+     */
+    public function testDeleteCacheValue(): void
+    {
+        // expect call delete cache value
+        $this->redis->expects($this->once())->method('__call')
+            ->with('del', ['test_key']);
+
+        // expect no error handling
+        $this->errorManager->expects($this->never())->method('handleError');
+
+        // call tested method
+        $this->cacheManager->deleteCacheValue('test_key');
+    }
+}
